585d955dc0dff487938706fa1a84c72afdedf6b7
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-06-06 22:10:31 +0200

    Added the hourly forecast and styled it

--- html-css-and-js/weather-app/src/components/weatherDisplayComponents/HourlyWeatherForecast.jsx
+++ html-css-and-js/weather-app/src/components/weatherDisplayComponents/HourlyWeatherForecast.jsx
@@ -1,6 +1,51 @@
 function HourlyWeatherForecast () {
   return (
-    <div></div>
+    <div>
+      <div className="flex items-center justify-between">
+        <p className="text-[hsl(0,0%,100%)]">
+          Hourly Forecast
+        </p>
+        <button className="bg-[hsl(243,23%,30%)] text-white flex items-center gap-2 cursor-pointer px-2 py-1 rounded-md">
+          <img src="./assets/images/icon-units.svg" alt="units icon" />
+          Tuesday
+          <img src="./assets/images/icon-dropdown.svg" alt="dropdown icon" />
+        </button>
+      </div>
+      <div className="grid gap-2 text-[hsl(0,0%,100%)]">
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+        <div className="bg-[hsl(243,23%,30%)] flex justify-between items-center">
+          <img className="w-8" src="./assets/images/icon-rain.webp" alt="weather icon" />
+          <span>20°C</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
